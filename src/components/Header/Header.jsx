af905e0303056ecf4c86f5d9a3b4aeb30fd01ff7
dmitridrozdov <dm.drozdov@gmail.com>
2021-08-20 12:09:17 +1000

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,12 @@ const Header = () => {
     return (
         <AppBar position='static'>
             <Toolbar className={classes.toolbar}>
-                <Typography variant='h5'></Typography>
+                <Typography variant='h5' className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display='flex'>
+
+                </Box>
             </Toolbar>
         </AppBar>
     )
