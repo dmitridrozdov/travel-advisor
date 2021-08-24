b36df1f3fc7b90cbc64a92dc864d615ada342585
dmitridrozdov <dm.drozdov@gmail.com>
2021-08-24 11:36:38 +1000

    Update Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
 const Map = () => {
+    const classes = useStyles()
+    const matches = useMediaQuery('(min-width:600px)')
     return (
         <h1>Map</h1>
     )
