ea06472271f8309d0aaf510b135d9e0cc5b3d9db
dmitridrozdov <dm.drozdov@gmail.com>
2021-08-24 11:49:02 +1000

    Update Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,10 +7,16 @@ import Rating from '@material-ui/lab/Rating';
 const Map = () => {
     const classes = useStyles()
     const matches = useMediaQuery('(min-width:600px)')
+    const coordinates = { lat: 0, lng: 0 }
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
                 bootstrapURLKeys={{ key: '' }}//process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
             >
 
             </GoogleMapReact>
