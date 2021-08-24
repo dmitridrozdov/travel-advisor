50d8024be29b066716c24955e0bd061adab88a3b
dmitridrozdov <dm.drozdov@gmail.com>
2021-08-24 16:46:03 +1000

    Update List.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,7 +3,17 @@ import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, 
 
 const List = () => {
     return (
-        <h1>List</h1>
+        <div className={classes.container}>
+            <Typography variant="h4">Food & Dining around you</Typography>
+            <FormControl className={classes.formControl}>
+                <InputLabel id="type">Type</InputLabel>
+                <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
     )
 }
 
