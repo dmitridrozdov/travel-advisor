b50b6d84f5351be0e3187c1aa1690130b6a3a327
dmitridrozdov <dm.drozdov@gmail.com>
2021-08-25 10:36:17 +1000

    Update List.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect, createRef } from 'react'
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core'
+import useStyles from './styles.js'
 
 const List = () => {
+    const [elRefs, setElRefs] = useState([])
+    const classes = useStyles()
+
     return (
         <div className={classes.container}>
             <Typography variant="h4">Food & Dining around you</Typography>
             <FormControl className={classes.formControl}>
                 <InputLabel id="type">Type</InputLabel>
-                <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
+                {/* <Select id="type" value={type} onChange={(e) => setType(e.target.value)}> */}
+                <Select value={} onChange={}>
                     <MenuItem value="restaurants">Restaurants</MenuItem>
                     <MenuItem value="hotels">Hotels</MenuItem>
                     <MenuItem value="attractions">Attractions</MenuItem>
