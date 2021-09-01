887fbaad126a11b3fd98e8dac9199262e19acf7d
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-01 16:07:35 +1000

    Update PlaceDetails.jsx

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core'
 
 const PlaceDetails = ({ place }) => {
-    console.log('console log from placedetails:' + place)
+    console.log(place) //do not delete ... to explore what else can be on the card
     return (
         <h1>   
           {place.name}  
