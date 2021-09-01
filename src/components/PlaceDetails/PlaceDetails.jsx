e4ed541dea85e420c773088489384518fd4e53a4
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-01 16:10:29 +1000

    Update PlaceDetails.jsx

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+import PhoneIcon from '@material-ui/icons/Phone'
+import Rating from '@material-ui/lab/Rating'
 
 const PlaceDetails = ({ place }) => {
     console.log(place) //do not delete ... to explore what else can be on the card
