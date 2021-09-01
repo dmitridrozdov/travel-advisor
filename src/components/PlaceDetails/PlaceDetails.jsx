e5e6b169c0d115eaebd2cb16e93a0f3c116626d9
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-01 16:13:00 +1000

    Update PlaceDetails.jsx

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -4,6 +4,8 @@ import LocationOnIcon from '@material-ui/icons/LocationOn'
 import PhoneIcon from '@material-ui/icons/Phone'
 import Rating from '@material-ui/lab/Rating'
 
+import useStyles from './styles.js'
+
 const PlaceDetails = ({ place }) => {
     console.log(place) //do not delete ... to explore what else can be on the card
     return (
