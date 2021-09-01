a315e441438982077414598af194ccf8a0cff249
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-01 12:54:35 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ const App = () => {
         if(bounds) {
             getPlacesData(bounds.sw, bounds.ne)
                 .then((data) => {
-                    console.log(data)
                     setPlaces(data)
                 })
         }
@@ -33,7 +32,7 @@ const App = () => {
             <Header />
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
-                    <List />
+                    <List places={places}/>
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Map 
