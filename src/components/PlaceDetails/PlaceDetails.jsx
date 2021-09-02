2e0ebf520995bfcb3fa968d5bee9835845d277a7
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-02 15:22:54 +1000

    Update PlaceDetails.jsx

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -16,6 +16,9 @@ const PlaceDetails = ({ place }) => {
             image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
             title={place.name}
           />
+        <CardContent>
+          <Typography gutterBottom variant="h5">{place.name}</Typography>
+        </CardContent>
         </Card>
     )
 }
