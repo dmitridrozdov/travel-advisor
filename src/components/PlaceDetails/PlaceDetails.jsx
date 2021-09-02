d7196f520570027aca529674270139527d2bc3e7
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-02 15:21:09 +1000

    Update PlaceDetails.jsx

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -13,6 +13,8 @@ const PlaceDetails = ({ place }) => {
         <Card elevation={6}>
           <CardMedia 
             style={{ height: 350 }}
+            image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+            title={place.name}
           />
         </Card>
     )
