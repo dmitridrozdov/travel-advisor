3d49678da9d7c7b2ba78b46fdfc5d22817ce07ad
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-15 15:58:40 +1000

    Update PlaceDetails.jsx

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -7,7 +7,7 @@ import Rating from '@material-ui/lab/Rating'
 import useStyles from './styles.js'
 
 const PlaceDetails = ({ place }) => {
-    console.log(place) //do not delete ... to explore what else can be on the card
+    // console.log(place) //do not delete ... to explore what else can be on the card
     const classes = useStyles()
     return (
         <Card elevation={6}>
