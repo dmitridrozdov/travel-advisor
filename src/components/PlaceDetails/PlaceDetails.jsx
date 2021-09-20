25be70f0480e31ed3c98699910ba57d3b27e2249
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-20 10:52:12 +1000

    Update PlaceDetails.jsx

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -7,6 +7,7 @@ import Rating from '@material-ui/lab/Rating'
 import useStyles from './styles.js'
 
 const PlaceDetails = ({ place, selected, refProp }) => {
+    if (selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' })
     // console.log(place) //do not delete ... to explore what else can be on the card
     const classes = useStyles()
     return (
