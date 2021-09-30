757f8615ca026f7be20a3851ef530775849fbe33
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-30 10:59:50 +1000

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,9 @@
 import axios from 'axios'
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
     try {
         const { data: { data } } = await axios.get(
-            URL, 
+            `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, 
             {
                 params: {
                   bl_latitude: sw.lat,
@@ -16,12 +14,30 @@ export const getPlacesData = async (sw, ne) => {
                 },
                 headers: {
                   'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-                  'x-rapidapi-key': ''
+                  'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY
                 }
             }    
         )
         return data
     } catch(error) {
         console.log(error)
+    }
+}
+
+export const getWeatherData = async (lat, lng) => {
+    try {
+      if (lat && lng) {
+        const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+          params: { lat, lon: lng },
+          headers: {
+            'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+            'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+          },
+        })
+  
+        return data
+      }
+    } catch (error) {
+      console.log(error)
     }
 }
