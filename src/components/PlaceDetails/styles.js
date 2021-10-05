796e701877c96d5131ed360613a68a345aa0175e
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-05 15:01:06 +1100

    Update styles.js

--- src/components/PlaceDetails/styles.js
+++ src/components/PlaceDetails/styles.js
@@ -3,23 +3,29 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(() => ({
   chip: {
     margin: '5px 5px 5px 0',
+    fontFamily: 'Arsenal',
+    fontSize: '14px',
   },
   subtitle: {
     display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: '10px',
+    fontFamily: 'Arsenal',
+    fontSize: '14px',
   },
   spacing: {
     display: 'flex', alignItems: 'center', justifyContent: 'space-between',
+    fontFamily: 'Arsenal',
+    fontSize: '14px',
   },
   placeName: {
     fontFamily: 'Handlee',
     fontSize: '24px',
   },
-  reviewsFont: {
+  cardFont: {
     fontFamily: 'Montserrat',
     fontSize: '14px',
   },
-  priceFont: {
-    fontFamily: 'Montserrat',
-    fontSize: '16px',
+  awardFont: {
+    fontFamily: 'Arsenal',
+    fontSize: '14px',
   },
 }));
